b671641da98555cdf0823ab545601e5f749e4c45
shaimeur <76816609+shaimeur@users.noreply.github.com>
2023-03-04 18:15:04 +0100

    adding some css with the import statement && outputting dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,18 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return(
-        <div>
-            <h3>ExpenseItem is working!</h3>
-        </div>
-    );
+  const expenseDate = new Date(2021, 2,28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem ;
+export default ExpenseItem;
