d6701a59dcd93e4b23871d16b9388fa811f7e6f7
shaimeur <76816609+shaimeur@users.noreply.github.com>
2023-03-12 15:17:05 +0100

    adding the solution projeded by the instractorgit add *

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,21 @@
-import React from "react";
-import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
-import ExpensesFilter from "./ExpensesFilter";
-import Card from "../UI/Card";
+import React, { useState } from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
 
-  const filterExpenseByYear = (result) =>{
-      console.log(result," : is the year from Expenses")
-  }
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
 
-return (
-  <div>
-    <Card className="expenses">
-      <ExpensesFilter onFilterExpense = {filterExpenseByYear} />
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
@@ -38,4 +40,5 @@ return (
     </div>
   );
 };
+
 export default Expenses;
