6a856d6ed5f51f255a7312feff8df9d695e80f3b
shaimeur <haimeursaad@gmail.com>
2023-03-16 14:35:01 +0100

    assingnment for the filter change

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,11 +6,15 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2021");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+   const filteredExpenses = props.items.filter((expense)=>{
+    return expense.date.getFullYear().toString() === filteredYear
+   })
 
   return (
     <div>
@@ -18,8 +22,9 @@ const Expenses = (props) => {
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
+          onChange = {filteredExpenses}
         />
-        {props.items.map((expense) => {
+        {filteredExpenses.map((expense) => {
           return (
             <ExpenseItem
               key={expense.id}
