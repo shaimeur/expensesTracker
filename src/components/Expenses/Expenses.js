9acd7c23ed25d1692a7db1aca6fa645ef313643b
shaimeur <haimeursaad@gmail.com>
2023-03-16 19:24:50 +0100

    veiwing the conditionale rendring in the template using ternory operator or handling is with just && operator

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,7 +15,20 @@ const Expenses = (props) => {
    const filteredExpenses = props.items.filter((expense)=>{
     return expense.date.getFullYear().toString() === filteredYear
    })
+   let expensesContent = <p>No expense Found!!</p>;
 
+   if(filteredExpenses.length > 0){
+        expensesContent =   filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    })
+   }
   return (
     <div>
       <Card className="expenses">
@@ -24,7 +37,8 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
           onChange = {filteredExpenses}
         />
-        {filteredExpenses.map((expense) => {
+          {expensesContent}
+        {/* {filteredExpenses.length === 0 ? <p>No expense found!</p>: filteredExpenses.map((expense) => {
           return (
             <ExpenseItem
               key={expense.id}
@@ -33,7 +47,7 @@ const Expenses = (props) => {
               date={expense.date}
             />
           );
-        })}
+        })} */}
       </Card>
     </div>
   );
